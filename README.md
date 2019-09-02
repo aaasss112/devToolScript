261359a8293a0f6443cf728d99559e7264563642
renqiangqiang <renqiangqiang@yy.com>
2019-09-02 15:22:06 +0800

    升级到Python3, 完成screencap命令

--- README.md
+++ README.md
@@ -1,12 +1,14 @@
 # devToolScript
 日常开发脚本
 ## before of all
-- `pip install -r requirement.txt`:项目根目录运行该命令，安装相关依赖
+- 所有Python脚本基于`Python3`,且只支持`Python3`
+- `pip3 install -r requirement.txt`:项目根目录运行该命令，安装相关依赖
 
 ### [adb.py](./adb.py)
-- `python adb.py install xx.apk`：同时在多台已连接的设备上安装apk
-- `python adb.py path "com.duowan.mobile""`：根据指定包名查询并显示apk的安装路径
-- `python adb.py path "com.duowan.mobile" ~/Desktop/`：同时把apk pull 到指定路径
+- `python3 adb.py install xx.apk`：同时在多台已连接的设备上安装apk
+- `python3 adb.py path "com.duowan.mobile""`：根据指定包名查询并显示apk的安装路径
+- `python3 adb.py path "com.duowan.mobile" ~/Desktop/`：同时把apk pull 到指定路径
+- `python3 adb.py screencap ~/Desktop`：获取连接设备的实时截图，多台设备连接时，会分别获取每台设备的截图
 
 ### [adbTop.sh](./adbTop.sh)
-- `./adbTop.sh "com.duowan.mobile"`：查看指定包名的Activity堆栈(若遇到`permission denied: adbTop.sh`，请先运行`chmod +x adbTop.sh`)
+- `./adbTop.sh "com.duowan.mobile"`：查看指定包名的Activity堆栈，可用于查看当前显示的Activity(若遇到`permission denied: adbTop.sh`，请先运行`chmod +x adbTop.sh`)
--- adb.py
+++ adb.py
@@ -1,17 +1,16 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
+#!/usr/bin/env python3
 r""" A command line tool for ADB
 
 Command Usage:
 
-install:  
+install:
     install local apk into multi devices at once.
 - python adb.py install xx.apk
 
 path:
     Print the installation path of the specified package name, and pull to specified path
 - python adb.py path "com.duowan.mobile"
-- python adb.py path "com.duowan.mobile" ~/Desktop/  
+- python adb.py path "com.duowan.mobile" ~/Desktop/
 
 """
 import subprocess
@@ -26,70 +25,118 @@ LOGGING_FORMAT = '%(asctime)s [%(levelname)s]: %(message)s'
 logging.basicConfig(format=LOGGING_FORMAT, level=logging.DEBUG)
 
 
+class NoDevicesConnectionException(Exception):
+    def __init__(self, output='当前没有已连接的设备，请确保设备连接后重试'):
+        self.output = output
+
+    def __str__(self):
+        return self.output
+
+
+def _get_connection_devices():
+    """获取已连接的设备id"""
+    devices = []
+    try:
+        result = subprocess.check_output(['adb', 'devices']).strip().decode()
+        devices = re.findall('(\w+)\t+device', result)
+    except:
+        pass
+    if len(devices) > 0:
+        return devices
+
+    raise NoDevicesConnectionException()
+
+
 def _multi_install(apk):
-    r""" install local apk into multi devices at once.  
+    r""" install local apk into multi devices at once.
 
     >>> python adb.py install xx.apk
     """
-
-    if not str(apk).endswith(".apk"):
+    if not apk.endswith(".apk"):
         logging.error("only support *.apk file")
         return
 
     try:
-        result = subprocess.check_output(['adb', 'devices']).strip()
-        reg_result = re.findall(r'(\w+)\t+device', result)
-        if reg_result:
-            for device_id in reg_result:
+        devices = _get_connection_devices()
+        for device in devices:
+            logging.info(
+                "installing apk: %s into device: %s ..." % (apk, device))
+            try:
+                subprocess.check_output(
+                    ['adb', '-s', device, 'install', '-r', '-d', apk])
                 logging.info(
-                    "installing apk: %s into device_id: %s ..." % (apk, device_id))
-                try:
-                    subprocess.check_call(
-                        ['adb', '-s', device_id, 'install', '-r', '-d', apk])
-                    logging.info(
-                        "install success... apk : %s, device_id = %s" % (apk, device_id))
-                except:
-                    print
-                    logging.error(
-                        "install failure... apk : %s, device_id = %s \n" % (apk, device_id))
-                    continue
-        else:
-            logging.error(
-                "There are not connected devices. Please check devices connection.")
+                    "install success... apk : %s, device = %s" % (apk, device))
+            except:
+                print
+                logging.error(
+                    "install failure... apk : %s, device = %s \n" % (apk, device))
+                continue
     except Exception as e:
         logging.error(e)
 
 
 def _path(pkg_name, pull=None):
     """Print the installation path of the specified package name, and pull to specified path.
-    
+
     >>> python adb.py path "com.duowan.mobile"  
     >>> python adb.py path "com.duowan.mobile" ~/Desktop/    
     """
+    devices = _get_connection_devices()
+    if len(devices) > 1:
+        logging.error('检测到多台设备连接，请确保只有一台设备连接后重试')
+        return
     try:
         logging.info("The package name: %s" % pkg_name)
         pkg_path = subprocess.check_output(
-            ['adb', 'shell', 'pm', 'path', pkg_name])
-        print pkg_path
+            ['adb', 'shell', 'pm', 'path', pkg_name]).decode()
+        print(pkg_path)
         if isinstance(pull, str):
             try:
                 path_reg = re.findall(r'.*:(.*)', pkg_path)
                 if path_reg and path_reg[0]:
                     logging.info("Being pull to : %s" % pull)
-                    subprocess.check_call(
+                    subprocess.check_output(
                         ['adb', 'pull', path_reg[0], pull])
                     os.rename(os.path.join(pull, "base.apk"),
                               os.path.join(pull, "%s.apk" % pkg_name))
-                    print "\npull to %s success, the apk name is %s.apk" % (
-                        pull, pkg_name)
+                    print("\npull to %s success, the apk name is %s.apk" % (
+                        pull, pkg_name))
             except Exception as e:
                 logging.error("\npull failure...error = %s" % e)
     except:
         logging.error("\nno result, please check input package name!")
 
 
+def _screen_shot(path):
+    """ 获取连接设备的实时截图，多台设备连接时，会分别获取每台设备的截图
+
+    >>> python3 adb.py screencap ~/Desktop
+    """
+    if not path:
+        logging.error('path invalid, please check the path.')
+        return
+    try:
+        devices = _get_connection_devices()
+        if len(devices) > 1:
+            logging.info('当前有多台已连接设备，会分别获取每台设备的实时屏幕截图，并以设备id命名')
+            for device in devices:
+                logging.info('start %s screen shot...' % device)
+                _screen_shot_name = '/sdcard/screen-shot-%s.png' % device
+                subprocess.check_call(
+                    ['adb', '-s', device, 'shell', 'screencap', '-p', _screen_shot_name])
+                subprocess.check_output(
+                    ['adb', '-s', device, 'pull', _screen_shot_name, path])
+                subprocess.check_call(
+                    ['adb', '-s', device, 'shell', 'rm', _screen_shot_name])
+                logging.info(
+                    'screen shot was successful, output path is: %s' % os.path.join(path, "screen-shot-%s.png" % device))
+    except:
+        logging.error('screen shot has occur some error, please retry... ')
+
+
 if __name__ == "__main__":
     fire.Fire({
         "install": _multi_install,
-        "path": _path
+        "path": _path,
+        'screencap': _screen_shot
     })
