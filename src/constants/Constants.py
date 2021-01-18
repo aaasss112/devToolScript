a75da2de9675d67905078aa145d09379cc0377b6
renqiangqiang <renqiangqiang@yy.com>
2021-01-18 14:23:03 +0800

    添加文件夹过滤

--- src/constants/Constants.py
+++ src/constants/Constants.py
@@ -1,4 +1,4 @@
 YY_ROOT_DIR = "/Users/renqiangqiang/AndroidWorkSpace/YY"
 EXCLUDE_DIR = ['yy_workspace_merge', 'yytoolkit', 'yylivekit-android', 'athliveroom-android', 'small-android', 'SimpleStartLiveModule', 'kinds',
                'AndroidCodeStyle', 'devToolScript', 'MyApplication', 'CodeScanPlugin', 'RN', 'livesdkdemo-android', 'sniper-android',
-               'rtengine', 'streamManager-android']
+               'rtengine', 'streamManager-android', 'athvideoeffect-android']
